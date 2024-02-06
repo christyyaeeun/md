6449585e97a4bfbe4ceb572129914d8f833026d0
christyyaeeun <christypark21@gmail.com>
2024-02-06 12:09:58 -0600

    Fixing analytics installation

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -6,7 +6,11 @@ const Layout = () => {
     return (
         <>
             <Box minW='350px' pt="2em" mt="4em">
-
+                <img
+                    src="https://i.liadm.com/s/81303?locationId=c1by6u2tTkBaf8eekpx1&formId=&url_path=&cbust=&gclid=&utm_source=&utm_medium=&utm_campaign=&s1=&s2=&s3=alt"
+                    style={ { display: 'none', important: 'true' } }
+                    alt="Description of the image"
+                />
                 <Outlet />
             </Box>
         </>
